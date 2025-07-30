763220ce552376dcdc368ed460bf4ffc73db6b0c
Mauricio Akito <mauricio94jap@hotmail.com>
2025-07-30 20:33:55 -0300

    fix(Card): Removed unused declaration

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import styles from './Card.module.scss'
 import { weatherCodeToText } from '../../utils/weatherCode'
 import type { ICardDataProps } from '../../types/Card'
