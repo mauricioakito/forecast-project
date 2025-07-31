2f280bdf5665f7823b53c7c5cd67b196005b403e
Mauricio Akito <mauricio94jap@hotmail.com>
2025-07-30 21:50:41 -0300

    fix(GeoService): removed console log

--- src/services/geoservice.ts
+++ src/services/geoservice.ts
@@ -4,8 +4,6 @@ import { _Maybe } from 'funcio';
 export const GeoService = async (address: string) => {
   const response = await fetchData(`https://nominatim.openstreetmap.org/search?q=${address}&format=json&limit=1`);
 
-  console.log(response)
-
   const geolocation = _Maybe.of(response)
   .map(response => response.data)
   .map(data => data && data.length > 0 && data[0])
