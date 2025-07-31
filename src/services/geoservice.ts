cadb59885ea1ce0b3c8de6233c7594ef05cc7b13
Mauricio Akito <mauricio94jap@hotmail.com>
2025-07-30 21:43:09 -0300

    fix(geoservice): Added variable checking

--- src/services/geoservice.ts
+++ src/services/geoservice.ts
@@ -4,6 +4,8 @@ import { _Maybe } from 'funcio';
 export const GeoService = async (address: string) => {
   const response = await fetchData(`https://nominatim.openstreetmap.org/search?q=${address}&format=json&limit=1`);
 
+  console.log(response)
+
   const geolocation = _Maybe.of(response)
   .map(response => response.data)
   .map(data => data && data.length > 0 && data[0])
@@ -11,8 +13,8 @@ export const GeoService = async (address: string) => {
 
   return {
     status: response.status,
-    hasData: !(response.data.length === 0),
-    latitude: geolocation.lat,
-    longitute: geolocation.lon,
+    hasData: !(response.data && response.data.length === 0),
+    latitude: geolocation.length > 0 && geolocation.lat,
+    longitute: geolocation.length > 0 && geolocation.lon,
   }
 }
