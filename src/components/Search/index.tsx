37749bac5a3cd77e331df8d06902de2787d04f4d
Mauricio Kitazawa <61957945+mauricioakito@users.noreply.github.com>
2025-08-06 00:23:29 -0300

    Refact/search fields (#2)
    * refact(SearchForm): Removed unused constraint
    
    * feat(Search): Changed component to use only one field address and tweaked search button behavior
    
    * styles(Search): Adjusted input field spacing and button border
    
    * refact(Home): Changed initial message
    
    * refact(SearchForm): Removed unused constraint

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,40 +1,27 @@
-import {
-  useCallback,
-  useState,
-  type ChangeEvent,
-} from "react";
+import { useState } from "react";
 import styles from "./Search.module.scss";
 import { GeoService } from "../../services/geoservice";
 import { ForecastService } from "../../services/forecastservice";
 import classNames from "classnames";
-import { MESSAGES, SEARCH_FORM } from "../../constraints/SearchForm";
+import { MESSAGES } from "../../constraints/SearchForm";
 import { useWeatherStore } from "../../store/useWeatherStore";
+import { FaCity } from "react-icons/fa6";
 
 export const Search = () => {
+  const { setCardData, setIsSearching, setStatus, isSearching } =
+    useWeatherStore();
 
-  const {
-    setCardData,
-    setIsSearching,
-    setStatus,
-    isSearching,
-  } = useWeatherStore();
-
-  const [inputText, setInputText] = useState({
-    street: "",
-    number: "",
-    postalCode: "",
-    city: "",
-  });
-
+  const [city, setCity] = useState("");
+ 
   const searchForecast = async () => {
     setIsSearching(true);
     setCardData(undefined);
-    const { street, number, postalCode, city } = inputText;
-    const geoServiceResponse = await GeoService(
-      `${number},${street},${city},${postalCode}`
-    );
+    const geoServiceResponse = await GeoService(city);
 
-    if ((!geoServiceResponse.hasData) || (geoServiceResponse.status >= 400 && geoServiceResponse.status < 500)) {
+    if (
+      !geoServiceResponse.hasData ||
+      (geoServiceResponse.status >= 400 && geoServiceResponse.status < 500)
+    ) {
       // This Timeout function is here to show the loading status while the request is finished. It is only for example purposes.
       setTimeout(() => {
         setIsSearching(false);
@@ -53,53 +40,47 @@ export const Search = () => {
 
     const forecastServiceResponse = await ForecastService(latitude, longitute);
 
-    if (forecastServiceResponse.status >= 400 && forecastServiceResponse.status < 500) {
+    if (
+      forecastServiceResponse.status >= 400 &&
+      forecastServiceResponse.status < 500
+    ) {
       setIsSearching(false);
       setStatus(MESSAGES.wheather[400]);
       return;
     }
 
-    if (forecastServiceResponse.status >= 500 && forecastServiceResponse.status < 600) {
+    if (
+      forecastServiceResponse.status >= 500 &&
+      forecastServiceResponse.status < 600
+    ) {
       setIsSearching(false);
       setStatus(MESSAGES.wheather[500]);
       return;
     }
 
     if (forecastServiceResponse) {
-      setStatus('');
+      setStatus("");
       setCardData([...forecastServiceResponse.data]);
       setIsSearching(false);
     }
   };
 
-  const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>, fieldName: string) => {
-    setInputText((prevState) => ({
-      ...prevState,
-      [fieldName]: event.target.value,
-    }));
-  }, []);
-
   return (
     <form className={styles.search}>
       <div className={styles.inputContainer}>
-        {SEARCH_FORM.map(({ Icon, placeholder, name }) => {
-          return (
-            <div className={styles.inputInnerContainer} key={name}>
-              <Icon className={styles.inputIcon} />
-              <input
-                className={styles.input}
-                type="text"
-                placeholder={placeholder}
-                onChange={(event) => handleChange(event, name)}
-              />
-            </div>
-          );
-        })}
-
+        <div className={styles.inputInnerContainer}>
+          <FaCity className={styles.inputIcon} />
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="City"
+            onChange={(event) => setCity(event.target.value)}
+          />
+        </div>
       </div>
       <button
         className={classNames(styles.button, {
-          [styles.disabledButton]: isSearching,
+          [styles.disabledButton]: isSearching || !city,
         })}
         onClick={searchForecast}
         disabled={isSearching}
