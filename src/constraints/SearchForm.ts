18e36874986d7d7acc104441abc2dc2c15b3df4f
Mauricio Akito <mauricio94jap@hotmail.com>
2025-08-06 00:21:50 -0300

    refact(SearchForm): Removed unused constraint

--- src/constraints/SearchForm.ts
+++ src/constraints/SearchForm.ts
@@ -1,8 +1,3 @@
-import { AiOutlineFieldNumber } from "react-icons/ai";
-import { BsMailboxFlag } from "react-icons/bs";
-import { FaHouse, FaCity } from "react-icons/fa6";
-import type { IconType } from "react-icons/lib";
-
 export const MESSAGES: {geolocation: {400: string, 500: string}, wheather: {400: string, 500: string}} = {
   geolocation: {
     400: "We couldn't locate a valid address based on your input. Please double-check the details you've provided and try searching again.",
@@ -13,26 +8,3 @@ export const MESSAGES: {geolocation: {400: string, 500: string}, wheather: {400:
     500: "We're experiencing technical difficulties with our weather data API at the moment. Please bear with us and try again a little later."
   }
 }
-
-export const SEARCH_FORM: {Icon: IconType, placeholder: string, name: string}[] = [
-  {
-    Icon: AiOutlineFieldNumber,
-    placeholder: 'Enter your number',
-    name: 'number'
-  },
-  {
-    Icon: FaHouse,
-    placeholder: 'Enter your street name',
-    name: 'street'
-  },
-  {
-    Icon: FaCity,
-    placeholder: 'Enter your city',
-    name: 'city'
-  },
-  {
-    Icon: BsMailboxFlag,
-    placeholder: 'Enter your postal code',
-    name: 'postalCode'
-  },
-]
